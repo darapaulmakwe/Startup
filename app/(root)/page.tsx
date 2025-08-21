1529310dead703f3a72145b10a0b218612d49eb1
darapaulmakwe <dpaulmakwe@gmail.com>
2025-08-21 18:00:37 -0400

    Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl">Home</h1>
+    <section className="purple_container pattern">
+      <h1 className="heading">Pitch your Startup, <br/> 
+      Connect With Entreupeneurs Like You!</h1>
+
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
+      </p>
+
+    </section>
+    
     </>
   );
 }
